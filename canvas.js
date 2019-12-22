ed2f70aa10c5f0e2ac39229a45d26f0bfc061841
w87498112 <87498112@qq.com>
2019-12-22 10:20:06 +0800

    ==

--- canvas.js
+++ canvas.js
@@ -1,8 +1,33 @@
 //创建一个画布
-const ctx = wx.createCanvasContext('myCanvas')
 
+
+const width = 750;
+const heigth = 1350;
+
+const canvas = document.getElementById("myCanvas");
+canvas.width = width;
+canvas.height = heigth;
+let ctx = canvas.getContext("2d");
 let bgimgurl = './img/11_bg.jpg';
+let base64Img = null;
 
+let img = new Image()
+img.src = bgimgurl;
+img.onload = function () {
+    ctx.drawImage(img, 0, 0, img.width, img.height)
+    // 设置字体
+    ctx.font = "36px bold 黑体";
+    // 设置颜色
+    ctx.fillStyle = "#000000";
+    // 设置水平对齐方式
+    ctx.textAlign = "center";
+    // 设置垂直对齐方式
+    ctx.textBaseline = "middle";
+    // 绘制文字（参数：要写的字，x坐标，y坐标）
+    ctx.fillText("王延琦", 178, 380);
 
-ctx.drawImage(Path, 0, 0, 400, 200)
-ctx.draw();
+    base64Img = ctx.toDataURL('image/jpeg');
+
+    console.log(base64Img)
+    
+}
