b2007a2eb9475adacc42277f112a824e06c4c92a
梁亚丽 <80268612@qq.com>
2019-12-23 20:06:30 +0800

    1
    1

--- canvas.js
+++ canvas.js
@@ -14,39 +14,40 @@ function canvasDrawImg(canl, bg, obj, name, callback) {
     img_bg.onload = function () {
         ctx.drawImage(img_bg, 0, 0, img_bg.width, img_bg.height)
         // 设置字体
-        ctx.font = "36px bold 黑体";
+        ctx.font = "40px bold 黑体";
         // 设置颜色
-        ctx.fillStyle = "#db9e91";
+        ctx.fillStyle = "#ffffff";
         // 设置水平对齐方式
         ctx.textAlign = "left";
         // 设置垂直对齐方式
         ctx.textBaseline = "middle";
         // 绘制文字（参数：要写的字，x坐标，y坐标）
-        ctx.fillText(name, 100, 380);
+        ctx.fillText(name, 120, 415);
         const Qingwidth = 570,
             Qingheight = 120;
         let img_1 = new Image();
         img_1.setAttribute("crossOrigin", 'anonymous')
         img_1.src = obj[0];
         img_1.onload = function () {
-            ctx.drawImage(img_1, 90, 500, Qingwidth, Qingheight)
+            ctx.drawImage(img_1, 90, 490, Qingwidth, Qingheight)
             let img_2 = new Image();
             img_2.setAttribute("crossOrigin", 'anonymous')
             img_2.src = obj[1];
             img_2.onload = function () {
-                ctx.drawImage(img_2, 90, 660, Qingwidth, Qingheight)
+                ctx.drawImage(img_2, 90, 655, Qingwidth, Qingheight)
                 let img_3 = new Image();
                 img_3.setAttribute("crossOrigin", 'anonymous')
                 img_3.src = obj[2];
                 img_3.onload = function () {
                     ctx.drawImage(img_3, 90, 820, Qingwidth, Qingheight)
-                    let img_logo = new Image();
-                    img_logo.setAttribute("crossOrigin", 'anonymous')
-                    img_logo.src = "./img/logo.png";
-                    img_logo.onload = function () {
-                        ctx.drawImage(img_logo, 20, 20, img_logo.width, img_logo.height)
-                        drawImg()
-                    }
+                    drawImg()
+                    // let img_logo = new Image();
+                    // img_logo.setAttribute("crossOrigin", 'anonymous')
+                    // img_logo.src = "./img/logo.png";
+                    // img_logo.onload = function () {
+                    //     ctx.drawImage(img_logo, 20, 20, img_logo.width, img_logo.height)
+                        
+                    // }
                 }
             }
         }
@@ -78,32 +79,32 @@ function canvasDrawImg2(canl, bg, obj, name, callback) {
     img_bg.onload = function () {
         ctx.drawImage(img_bg, 2, topY, img_bg.width, img_bg.height)
         // 设置字体
-        ctx.font = "36px bold 黑体";
+        ctx.font = "40px bold 黑体";
         // 设置颜色
-        ctx.fillStyle = "#db9e91";
+        ctx.fillStyle = "#ffffff";
         // 设置水平对齐方式
         ctx.textAlign = "left";
         // 设置垂直对齐方式
         ctx.textBaseline = "middle";
         // 绘制文字（参数：要写的字，x坐标，y坐标）
-        ctx.fillText(name, 10, 72);
+        ctx.fillText(name, 30, 138);
         const Qingwidth = 605,
             Qingheight = 130;
         let img_1 = new Image();
         img_1.setAttribute("crossOrigin", 'anonymous')
         img_1.src = obj[0];
         img_1.onload = function () {
-            ctx.drawImage(img_1, 2, 100 + topY, Qingwidth, Qingheight)
+            ctx.drawImage(img_1, 2, 102 + topY, Qingwidth, Qingheight)
             let img_2 = new Image();
             img_2.setAttribute("crossOrigin", 'anonymous')
             img_2.src = obj[1];
             img_2.onload = function () {
-                ctx.drawImage(img_2, 2, 260 + topY, Qingwidth, Qingheight)
+                ctx.drawImage(img_2, 2, 276 + topY, Qingwidth, Qingheight)
                 let img_3 = new Image();
                 img_3.setAttribute("crossOrigin", 'anonymous')
                 img_3.src = obj[2];
                 img_3.onload = function () {
-                    ctx.drawImage(img_3, 0, 420 + topY, Qingwidth, Qingheight)
+                    ctx.drawImage(img_3, 2, 450 + topY, Qingwidth, Qingheight)
                     drawImg()
                 }
             }
